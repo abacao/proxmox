dd305b95dfc92e3bd1928db19e16222218e0ca84
André Bação <17479246+abacao@users.noreply.github.com>
2021-08-29 17:34:09 +0100

    docker and portainer

--- docker.sh
+++ docker.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-apt-get update && apt-get upgrade -y
+# Docker
+apt-get update && apt upgrade -y && apt dist-upgrade -y
 
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/deb... | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-apt-key fingerprint 0EBFCD88
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
 
-apt-get update && apt-get install docker-ce -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 systemctl enable docker
+
+# Portainer
+
+export PORTAINER_MNT=/mounts/portainer/data
+
+mkdir -p $PORTAINER_MNT
+
+docker run -d -p 9000:9000 --name=portainer --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v $PORTAINER_MNT:/data portainer/portainer-ce
